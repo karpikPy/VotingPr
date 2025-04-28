acf523e4ebb4b73ea9d89984baf3b995e16e8f64
Ura12342 <ura050593@gmail.com>
2025-04-28 19:45:02 +0300

    Update endpoints.py

--- backend/API/endpoints.py
+++ backend/API/endpoints.py
@@ -1,20 +1,20 @@
-
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends, HTTPException, status, Form
 from sqlalchemy.orm import Session, joinedload
-from backend.database.database import get_db
-from backend.API.schemas import (
+from database.database import get_db
+from API.schemas import (
     ChatMessageBase, PollOptionBase, UserCreate, UserLogin, Token,
     PollCreate, PollOptionCreate, VoteCreate, Poll as PollSchema, PollOption as PollOptionSchema,
-    User as UserSchema 
+    User as UserSchema
 )
-from backend.API.models import ChatMessage, User, Poll, PollOption
-from backend.auth.auth import get_password_hash, verify_password, create_access_token, get_user_by_email, get_current_user, get_current_admin_user
+from API.models import ChatMessage, User, Poll, PollOption
+from auth.auth import get_password_hash, verify_password, create_access_token, get_user_by_email, get_current_user, get_current_admin_user
 
 router = APIRouter()
 
 
 
-@router.get("/users/me", response_model=UserSchema) 
+
+@router.get("/users/me", response_model=UserSchema)
 async def read_users_me(current_user: User = Depends(get_current_user)):
     return current_user
 
@@ -34,8 +34,12 @@ def get_poll(poll_id: int, db: Session = Depends(get_db)):
 
 
 @router.post("/polls", response_model=PollSchema)
-def create_poll(poll: PollCreate, db: Session = Depends(get_db)):
-    db_poll = Poll(title=poll.title)
+async def create_poll(
+    poll: PollCreate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user) 
+):
+    db_poll = Poll(title=poll.title, creator_id=current_user.id) 
     db.add(db_poll)
     db.commit()
     db.refresh(db_poll)
@@ -58,16 +62,21 @@ def create_poll_option(poll_id: int, option: PollOptionBase, db: Session = Depen
 @router.put("/polls/{poll_id}", response_model=PollSchema)
 async def update_poll(
     poll_id: int,
-    poll_update: PollCreate, 
+    poll_update: PollCreate,
     db: Session = Depends(get_db),
-    current_admin_user: User = Depends(get_current_admin_user)
+    current_user: User = Depends(get_current_user) 
 ):
     db_poll = db.query(Poll).filter(Poll.id == poll_id).first()
     if db_poll is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Опитування не знайдено")
 
-    db_poll.title = poll_update.title
+    if not current_user.is_admin and db_poll.creator_id != current_user.id:
+         raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Operation not permitted",
+        )
 
+    db_poll.title = poll_update.title
     db.commit()
     db.refresh(db_poll)
     return db_poll
@@ -76,26 +85,34 @@ async def update_poll(
 @router.put("/polls/options/{option_id}", response_model=PollOptionSchema)
 async def update_poll_option(
     option_id: int,
-    option_update: PollOptionBase, 
+    option_update: PollOptionBase,
     db: Session = Depends(get_db),
-    current_admin_user: User = Depends(get_current_admin_user)
+    current_user: User = Depends(get_current_user) 
 ):
-    db_option = db.query(PollOption).filter(PollOption.id == option_id).first()
+    db_option = db.query(PollOption).options(joinedload(PollOption.poll)).filter(PollOption.id == option_id).first()
     if db_option is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Опція опитування не знайдена")
 
-    db_option.text = option_update.text
+    db_poll = db_option.poll
 
+    if not current_user.is_admin and db_poll.creator_id != current_user.id:
+         raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Operation not permitted",
+        )
+
+    db_option.text = option_update.text
     db.commit()
     db.refresh(db_option)
     return db_option
+
 
 
 @router.post("/votes/", response_model=PollOptionSchema)
 async def vote_for_option(
     vote: VoteCreate,
     db: Session = Depends(get_db),
-    current_user: User = Depends(get_current_user) 
+    current_user: User = Depends(get_current_user)
 ):
     db_option = db.query(PollOption).filter(PollOption.id == vote.poll_option_id).first()
     if db_option is None:
@@ -106,60 +123,26 @@ async def vote_for_option(
     db.refresh(db_option)
     return db_option
 
-
 @router.delete("/polls/{poll_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_poll(
     poll_id: int,
     db: Session = Depends(get_db),
-    current_admin_user: User = Depends(get_current_admin_user) 
+    current_user: User = Depends(get_current_user) 
 ):
     db_poll = db.query(Poll).filter(Poll.id == poll_id).first()
     if db_poll is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Опитування не знайдено")
 
+    if not current_user.is_admin and db_poll.creator_id != current_user.id:
+         raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Operation not permitted",
+        )
+
     db.delete(db_poll)
     db.commit()
 
-
     return {"detail": "Опитування успішно видалено"}
-
-
-
-
-@router.get("/messages/{other_user_id}", response_model=list[ChatMessageBase])
-async def get_messages_between_users(
-    other_user_id: int,
-    db: Session = Depends(get_db),
-    current_user: User = Depends(get_current_user) 
-):
-
-    current_user_id = current_user.id
-
-    messages = db.query(ChatMessage).filter(
-        ((ChatMessage.sender_id == current_user_id) & (ChatMessage.receiver_id == other_user_id)) |
-        ((ChatMessage.sender_id == other_user_id) & (ChatMessage.receiver_id == current_user_id))
-    ).order_by(ChatMessage.timestamp).all() 
-
-    return messages
-
-@router.post("/send_message", response_model=ChatMessageBase)
-async def send_message(message: ChatMessageBase, db: Session = Depends(get_db)):
-    new_message = ChatMessage(
-        sender_id=message.sender_id,
-        receiver_id=message.receiver_id,
-        content=message.content
-    )
-    db.add(new_message)
-    db.commit()
-    db.refresh(new_message)
-    return new_message
-
-
-@router.get("/get_messages", response_model=list[ChatMessageBase])
-async def get_messages(db: Session = Depends(get_db)):
-    messages = db.query(ChatMessage).all()
-    return messages
-
 
 
 
@@ -173,6 +156,38 @@ async def websocket_chat(websocket: WebSocket, client_id: int):
     except WebSocketDisconnect:
         await websocket.close()
         print(f"Клієнт {client_id} відключився")
+
+@router.post("/send_message", response_model=ChatMessageBase)
+async def send_message(message: ChatMessageBase, db: Session = Depends(get_db)):
+    new_message = ChatMessage(
+        sender_id=message.sender_id,
+        receiver_id=message.receiver_id,
+        content=message.content
+    )
+    db.add(new_message)
+    db.commit()
+    db.refresh(new_message)
+    return new_message
+
+@router.get("/get_messages", response_model=list[ChatMessageBase])
+async def get_messages(db: Session = Depends(get_db)):
+    messages = db.query(ChatMessage).all()
+    return messages
+
+@router.get("/messages/{other_user_id}", response_model=list[ChatMessageBase])
+async def get_messages_between_users(
+    other_user_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    current_user_id = current_user.id
+
+    messages = db.query(ChatMessage).filter(
+        ((ChatMessage.sender_id == current_user_id) & (ChatMessage.receiver_id == other_user_id)) |
+        ((ChatMessage.sender_id == other_user_id) & (ChatMessage.receiver_id == current_user_id))
+    ).order_by(ChatMessage.timestamp).all()
+
+    return messages
 
 
 @router.post("/register", response_model=Token)
